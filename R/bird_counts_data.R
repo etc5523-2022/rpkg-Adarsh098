0a9210a56e7ab74d15166479593e1ee9609a15b3
Adarsh098 <amor0060@student.monash.edu>
2022-09-29 18:50:08 +1000

    imported necessary r packages, created first function and added documentation for data.

---
+++ R/bird_counts_data.R
@@ -0,0 +1,9 @@
+
+#' @title The bird_counts dataset
+#'
+#' @description This data shows the numerous bird species counted by the birdwatchers in Hamilton area of Ontario, every year since Christmas 1921.
+#'
+#' @source https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-18/bird_counts.csv
+"bird_counts"
+
+
---
+++ R/most_birds.R
@@ -0,0 +1,12 @@
+
+#' @title Most birds counted during Christmas.
+#' @description A function to view most birds counted.
+#' @export
+most_birds <- function() {
+ bird_counts %>%
+    dplyr::group_by(species) %>%
+    dplyr::summarize(how_many_counted = sum(how_many_counted)) %>%
+    dplyr::arrange(desc(how_many_counted)) %>%
+    dplyr::slice(1:5)
+}
+
