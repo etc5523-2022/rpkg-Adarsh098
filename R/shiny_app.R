c4b1ddba1b910ab46a2265ffbdff0dd3e5c8de5f
Adarsh098 <amor0060@student.monash.edu>
2022-10-03 01:19:30 +1100

    renamed the function for shiny app

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -1,8 +1,8 @@
 
 #' @title Shiny Application
-#' @description This functions runs the shiny application created for this package.
+#' @description This functions runs the shiny application created for the rbirds.pkg package.
 #' @export
-shiny_app <- function() {
+run_shiny_app <- function() {
   app_dir <- system.file("shiny_adarsh098", package = "rbirds.pkg")
   shiny::runApp(app_dir, display.mode = "normal")
 }
