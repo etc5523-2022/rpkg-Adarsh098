edc603851dafdf0cf4c27b4c0fa296a693dbd0cf
Adarsh098 <amor0060@student.monash.edu>
2022-10-03 13:42:30 +1100

    created parameterised function and its testing script, added documentation for subset data files

--- R/hourly_data.R
+++ R/hourly_data.R
@@ -1,4 +1,5 @@
 
 #' @title The hourly_data data set
 #' @description This data set is a subset of the bird_counts data set and it shows an approximate range of hours required to count birds. This data was created for the function count_by_hours.
+"hourly_data"
 
---
+++ R/top_five_birds_yearly.R
@@ -0,0 +1,14 @@
+
+#' @title Top five birds counted yearly
+#' @description This function takes year as an input and allows user to view the top five birds counted in that year. It is also a parameterised version of the output of panel four of the shiny application of this package where the input "year" corresponds to the argument "num" in the function.
+#' @param num A numeric input of year between 1921 - 2017.
+#' @examples
+#' top_five_birds_yearly(1960)
+#' @export
+top_five_birds_yearly <- function(num) {
+  if (nrow(yearly_data %>% filter(year == num)) > 0) {
+    return (yearly_data %>% filter(year == num))
+  } else {
+    return (NULL)
+  }
+}
